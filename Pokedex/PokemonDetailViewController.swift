de198cf61c184d6f79d08dd5f68a0f23dd63db7c
Nathan Visser <contact@nathanvisser.dev>
2020-12-13 18:30:29 -0500

    created initial interface containing both main & detail screens

---
+++ Pokedex/PokemonDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PokemonDetailViewController.swift
+//  Pokedex
+//
+//  Created by Field Employee on 12/13/20.
+//
+
+import UIKit
+
+class PokemonDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
